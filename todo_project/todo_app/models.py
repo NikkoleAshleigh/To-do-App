acef9f6ffd07f5bbe73505aebd10effdccd8208c
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-12-13 18:30:13 -0500

    created and modified the to-do app as done through class

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
 
 class Task(models.Model):
     '''A task object will have a description of the task to complete'''
     description = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag)
 
 class Comment(models.Model):
     '''A comment object will have a description of the task to comment on, and will show the date and time it is creates at'''
@@ -12,3 +15,4 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
